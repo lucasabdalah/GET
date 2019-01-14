fba6d297790b4207b0f1ee3822a58c3963781511
Lucas Abdalah <39392860+lucasabdalah@users.noreply.github.com>
2019-01-14 19:34:05 -0200

    Função de Verificação das Matrizes
    - Utilizado na decomposição LU;
    - Utilizado na decomposição QR.

---
+++ verificacao.m
@@ -0,0 +1,26 @@
+%%%%%%%%% VERIFICACAO DE MATRIZ %%%%%%%%
+% Autor: Lucas Abdalah
+% Last Update: 14 jan 2019 
+% Verifica se a entrada e matriz
+
+function [aux] =  verificacao(U)
+    [m,n]=size(U); % Dimensoes da entrada
+    
+    %%%%%%%%% Bloco de Verificao  %%%%%%%%%
+    if m==n
+        if m*n>1 % Verifica se e matriz
+            %%%%%%%%% VERDADEIRO  %%%%%%%%%
+            aux=true;
+    	
+    	else
+            %%%%%%%%% FALSO - CASO 1  %%%%%%%%%
+        	aux=false; % A entrada nao e matriz
+        	return
+        end
+        
+    else
+    	%%%%%%%%% FALSO - CASO 2  %%%%%%%%%
+    	aux=false; % A entrada nao e matriz
+    	return
+    end
+end
