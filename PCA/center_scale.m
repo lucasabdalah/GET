f7d12dba4e8aad3151f2809fd7d188c1c35638be
Lucas Abdalah <39392860+lucasabdalah@users.noreply.github.com>
2020-04-27 12:42:43 +0200

    Update center_scale.m

--- PCA/center_scale.m
+++ PCA/center_scale.m
@@ -1,19 +1,31 @@
-function y = center_scale(x)
-%CENTER_SCALE on matrix data
-%     [y] = center_scale(x) returns a matrix centralized 
-%     and scaled. The y matrix is the x, but with zero mean
-%     and one of standart deviation.
+function XNormalized = center_scale(X)
+% Computes z-score data normalization (and standardization) of input signal(s)
 %
+% SYNTAX: XNormalized = center_scale(X);
+%
+%         XNormalized : input signals with zero mean and std one
+%
+%         X  : input signals (one signal per row, one sample per column).
+%
+% HISTORY:
+%
+% 2019/04/30: - created by Lucas Abdalah.
 
-% Measure Standart Deviation
-    x_std=std(x);
-% Measure the mean
-    x_mean=mean(x);    
-% Matrix with zeros, same size of x's matrix
-n = size(x,2);
-y = x.*0;
-%
-for ii=1:n
-   y(:,ii)=(x(:,ii) - x_mean(ii))/x_std(ii);
-end 
+X_std=std(X,0,2); % Standart Deviation by rows
+
+if X_std <= eps & X_std >= -eps
+  error("Stardand Deviation should not be zero");
+end
+
+X_mean=mean(X,2); % Mean by rows
+XNormalized = X - X_mean; % Shift the samples to the zero mean
+
+numLines = size(X,1); % Upper limit in the loop
+
+for indexLines=1:numLines
+   XNormalized(indexLines,:) = ...
+    XNormalized(indexLines,:)/X_std(indexLines,:);
+    % Divide each elemments for its line std
+end
+
 end
